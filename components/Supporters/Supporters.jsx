d5c1e9460d0e97b8f78359efc8a277c8990d0108
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-04-17 21:52:22 -0300

    finish supporters component with supporter svg mocks

--- components/Supporters/Supporters.jsx
+++ components/Supporters/Supporters.jsx
@@ -3,6 +3,29 @@ import {
   ImageWithLink
 } from '..'
 
+var supporters = [
+  {
+    src: "/supporters/papito.svg",
+    description: "Vercel Logo"
+  },
+  {
+    src: "/supporters/papito2.svg",
+    description: "Vercel Logo"
+  },
+  {
+    src: "/supporters/papito3.svg",
+    description: "Vercel Logo"
+  },
+  {
+    src: "/supporters/parangole.svg",
+    description: "Vercel Logo"
+  },
+  {
+    src: "/supporters/parangs.svg",
+    description: "Vercel Logo"
+  },
+]
+
 const Supporters = () => {
   return (
     <div className={styles.supportersContainer}>
@@ -16,6 +39,11 @@ const Supporters = () => {
           </h3>
           <ImageWithLink link="https://github.com/Hemocione" imagePath="/github.svg" />
         </div>
+      </div>
+      <div className={styles.supportersImagesContainer}>
+        {supporters.map(({src, description, ...rest}) => (
+          <img src={src} alt={description} className={styles.supporter} />
+        ))}
       </div>
     </div>
   )
