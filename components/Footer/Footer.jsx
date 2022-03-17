33c99c3561fa140c3b07815d5297cea7593ff36c
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-16 23:56:54 -0300

    img -> Image + competitions on footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,12 +1,13 @@
 import { FooterContacts, FooterMenu } from '..'
 import styles from './Footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => (
   <footer>
     <div className={styles.hills} />
     <div className={styles.footerContentContainer}>
       <div className={styles.footerLeftContainer}>
-        <img alt='Hemocione White Logo' className={styles.hemoLogo} src='/whiteLogo.svg' />
+        <Image alt='Hemocione White Logo' className={styles.hemoLogo} height={200} width={200} src='/whiteLogo.svg' />
         <FooterMenu />
       </div>
       <FooterContacts />
@@ -15,7 +16,7 @@ const Footer = () => (
       <p>© 2021 Hemocione</p>
       <div className={styles.vercelContainer}>
         <p className={styles.poweredBy}>Powered by</p>
-        <img alt='Vercel Logo' className={styles.vercelLogo} src='/vercel.svg' />
+        <Image alt='Vercel Logo' height={50} width={50} className={styles.vercelLogo} src='/vercel.svg' />
       </div>
       <p>Created by Hemocione</p>
     </div>
