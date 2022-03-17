bea91544c2502a6565c9eb1c4800ee0665d5cf7d
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-16 23:45:03 -0300

    better menu items + competitions

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import { MenuItems } from '..'
 import styles from './Navbar.module.css'
+import Image from 'next/image'
 
 const Navbar = () => (
   <nav className={styles.header}>
-    <img alt='Hemocione' className={styles.headerTitle} src='/title.svg' />
+    <Image alt='Hemocione' width={300} height={200} className={styles.headerTitle} src='/title.svg' />
     <MenuItems />
     {/* <LoginItems /> */}
   </nav>
