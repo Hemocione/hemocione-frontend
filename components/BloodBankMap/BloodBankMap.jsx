0bfdf4f8a5c5ad217a76df89b15561e895b32e2a
Luis Felipe Almeida <luisfelipealmeida@MacBook-Pro-de-Luis.local>
2022-07-05 21:49:58 -0300

    feat: add powered by in iframe

--- components/BloodBankMap/BloodBankMap.jsx
+++ components/BloodBankMap/BloodBankMap.jsx
@@ -1,7 +1,7 @@
+import Link from 'next/link'
 import styles from './BloodBankMap.module.css'
 
 const BloodBankMap = () => {
-
   return (
     <div className={styles.container}>
       <h2 id="ondedoar" className={styles.title}>
@@ -11,6 +11,12 @@ const BloodBankMap = () => {
       </h2>
       <div className={styles.iframeContainer}>
         <iframe className={styles.map} loading="lazy" src="https://www.google.com/maps/d/u/0/embed?mid=1s8gU7TwBgBVbE9fO3S7yUfrKP8Yhodiy" width="80%" height="640"/>
+        <div className={styles.poweredBy}>
+          Powered by
+          <span className={styles.red}>
+          <Link href="https://salvovidas.com"> Salvo Vidas </Link>
+          </span>
+        </div>
       </div>
     </div>
   )
