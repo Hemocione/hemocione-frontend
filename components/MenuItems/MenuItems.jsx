fcf1d78ccc140b1a9c05917ecd24fb241d45006d
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2023-10-07 14:11:25 -0300

    remove loja from menu

--- components/MenuItems/MenuItems.jsx
+++ components/MenuItems/MenuItems.jsx
@@ -5,7 +5,7 @@ const MenuItems = () => (
   <ul className={styles.menu}>
     <MenuItem link='/' text='Home' />
     <MenuItem link='/who' text='Quem Somos' />
-    <MenuItem external link='https://loja.hemocione.com.br/' text='Loja' />
+    {/* <MenuItem external link='https://loja.hemocione.com.br/' text='Loja' /> */}
     <MenuItem external link={process.env.NEXT_PUBLIC_COMPETITIONS} text='Competições' />
   </ul>
 )
