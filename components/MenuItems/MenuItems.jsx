95eac02e7bf019db185e59b4ae7bf3ca08c0db32
Luiz Eduardo <doginuwu@gmail.com>
2024-02-27 15:22:49 -0300

    feat: add missing menu item (#33)

--- components/MenuItems/MenuItems.jsx
+++ components/MenuItems/MenuItems.jsx
@@ -7,6 +7,7 @@ const MenuItems = () => (
     <MenuItem link='/who' text='Quem Somos' />
     {/* <MenuItem external link='https://loja.hemocione.com.br/' text='Loja' /> */}
     <MenuItem external link='https://eventos.hemocione.com.br' text='Eventos' />
+    <MenuItem external link='https://apoie.hemocione.com.br' text='Apoie' />
     {/* <MenuItem external link={process.env.NEXT_PUBLIC_COMPETITIONS} text='Competições' /> */}
   </ul>
 )
